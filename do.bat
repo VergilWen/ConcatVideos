3247e684423fc47fbe594b8055208143ab7b96de
VergilWen <357621678@qq.com>
2021-02-15 17:44:15 +0800

    ConcatVideos

---
+++ do.bat
@@ -0,0 +1,1 @@
+python ./src/auto_concat.py
---
+++ src/auto_concat.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import re
+parentDir = str.replace(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), "\\", "/")
+curDir = str.replace(os.path.dirname(__file__), "\\", "/")
+
+
+def GenFileList():
+    ftxtfile = open(str.format("{0}/filelist.txt", curDir), 'w')
+    list_dirs = os.walk(str.format("{0}/videos", parentDir))
+    for _, _, files in list_dirs:
+        for f in files:
+            if f.endswith(".mp4") or f.endswith(".ts"):
+                fileStr = str.format("file \'{0}/videos/{1}\'\n", parentDir, f)
+                ftxtfile.write(fileStr)
+
+def ConcatVideos():
+    cmd = str.format("{0}/ffmpeg.exe -y -f concat -safe 0 -i {0}/filelist.txt -c copy {1}/{2}.mp4", curDir, parentDir, GenOutputName())
+    cmd = str.replace(cmd, "/", "\\")
+    print(cmd)
+    os.system(cmd)
+
+def GenOutputName():
+    list_dirs = os.walk(str.format("{0}/videos", parentDir))
+    name = "output"
+    for _, _, files in list_dirs:
+        if len(files) > 1:
+            fileName = files[0]
+            match = re.search(".*\d{4}\d{2}\d{2}", fileName)
+            if match:
+                name = match.group(0)
+                break;
+    return name
+
+GenFileList()
+ConcatVideos()
